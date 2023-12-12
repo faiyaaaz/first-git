26a0b4a746ed74edccf634e9974d573f0b57f79a
Faiyaz Rahman <faiyaaaz.rahman@gmail.com>
2023-12-12 15:49:33 +0600

    prime-added

--- test.py
+++ test.py
@@ -1,12 +1,25 @@
-def facto(a):
-    factorial = 1
+# def facto(a):
+#     factorial = 1
 
-    if a == 0:
-        factorial = 1
-        return factorial
-    for i in range(1,a+1):
-        factorial = factorial * i
+#     if a == 0:
+#         factorial = 1
+#         return factorial
+#     for i in range(1,a+1):
+#         factorial = factorial * i
 
-    return factorial
+#     return factorial
 
-print(facto(int(input("Enter: "))))
+# print(facto(int(input("Enter: "))))
+
+def is_prime(a):
+    flag = True
+    if a == 1 :
+        flag = False
+        return flag
+    for i in range(2,a):
+        if a % i == 0:
+            flag = False
+    return flag
+
+print(is_prime(10))
+    
